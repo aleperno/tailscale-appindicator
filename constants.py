ce8eb9b6f8b51d11446a916893c624b95127434e
Alejandro Pernin <ale.pernin@gmail.com>
2025-06-12 18:35:42 -0300

    Change connect method
    I've found there's an instance when tailscale asks to reset previous
    configurations.

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
 TAILSCALE_STATUS = "tailscale status --json"
-TAILSCALE_CONNECT = "tailscale up --accept-routes=true"
+TAILSCALE_CONNECT = "tailscale up --reset --accept-routes=true"
 TAILSCALE_DISCONNECT = "tailscale down"
